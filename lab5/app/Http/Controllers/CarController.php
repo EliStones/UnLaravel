2fb32c0d0a41a11ab557421016a14e7a091d1add
Elisha Mbadi <64102946+EliStones@users.noreply.github.com>
2020-07-19 09:23:11 +0300

    Lab 5

---
+++ lab5/app/Http/Controllers/CarController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Car;
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Show all cars
+        $cars = Car::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        // Create a car
+        $make = $request->input('make');
+        $model = $request->input('model');
+        $produced_on = $request->input('produced-on');
+
+        $car = new Car(
+            // 'make' => $make,
+            // 'model' => $model,
+            // 'produced_on' => $produced_on,
+        );
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Car $car)
+    {
+        // Show a single car
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Car $car)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Car $car)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Car $car)
+    {
+        //
+    }
+}
